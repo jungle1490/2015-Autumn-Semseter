6fe366797309a3e26c47a585c2ae0709640301fa
joejungle <jungle1490@gmail.com>
2016-04-11 23:49:22 +0800

    #2

---
+++ HW2_02151202/src/HW2_02151202/HW2_02151202.java
@@ -0,0 +1,23 @@
+package HW2_02151202;
+//eco3_02151202_Jungle Cheng
+public class HW2_02151202 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 50;
+		//define a
+
+		System.out.println(a);
+		System.out.println(a--);
+		//a -= 1 after running the program, so the value will still be 50.
+		// We can say that we minus 1 at here.
+		System.out.println(a);
+		//This will become 49, cause we minus 1 after we run the program at line 12.
+		System.out.println(--a);
+		// a -= 1 before running the program, so this will be 48.
+		System.out.println(a);
+		
+	}
+
+}
